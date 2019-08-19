1595f16ae7748cd67ecab60b95624685df5fd52e
Mike Dickey <mdickey@splunk.com>
2019-08-19 12:33:53 -0700

    More whitespace changes in splunk/splunk entrypoint.sh file to fix ascii art formatting and finish indenting consistency.
    Merged in same changes for running as splunk user to universal
    forwarder Dockerfile and entrypoint.sh.
    
    Updated whitespace in universal forwarder entrypoint.sh to also use
    tables for consistency.

--- splunk/common-files/entrypoint.sh
+++ splunk/common-files/entrypoint.sh
@@ -56,10 +56,10 @@ watch_for_failure(){
 	echo Ansible playbook complete, will begin streaming var/log/splunk/splunkd_stderr.log
 	echo
 	user_permission_change
-	# Any crashes/errors while Splunk is running should get logged to splunkd_stderr.log and sent to the container's stdout
 	if [ `whoami` != "${SPLUNK_USER}" ]; then
 		RUN_AS_SPLUNK="sudo -u ${SPLUNK_USER}"
 	fi
+	# Any crashes/errors while Splunk is running should get logged to splunkd_stderr.log and sent to the container's stdout
 	if [ -z "$SPLUNK_TAIL_FILE" ]; then
 		${RUN_AS_SPLUNK} tail -n 0 -f ${SPLUNK_HOME}/var/log/splunk/splunkd_stderr.log &
 	else
@@ -69,13 +69,13 @@ watch_for_failure(){
 }
 
 create_defaults() {
-		createdefaults.py
+	createdefaults.py
 }
 
 start_and_exit() {
 	if [ -z "$SPLUNK_PASSWORD" ]
 	then
-			echo "WARNING: No password ENV var.  Stack may fail to provision if splunk.password is not set in ENV or a default.yml"
+		echo "WARNING: No password ENV var.  Stack may fail to provision if splunk.password is not set in ENV or a default.yml"
 	fi
 	sh -c "echo 'starting' > ${CONTAINER_ARTIFACT_DIR}/splunk-container.state"
 	setup
@@ -111,42 +111,42 @@ user_permission_change(){
 
 help() {
 	cat << EOF
-	____        _             _      __
+  ____        _             _      __
  / ___| _ __ | |_   _ _ __ | | __  \ \\
  \___ \| '_ \| | | | | '_ \| |/ /   \ \\
-	___) | |_) | | |_| | | | |   <    / /
+  ___) | |_) | | |_| | | | |   <    / /
  |____/| .__/|_|\__,_|_| |_|_|\_\  /_/
-			 |_|
+       |_|
 ========================================
 
 Environment Variables:
-	* SPLUNK_USER - user under which to run Splunk (default: splunk)
-	* SPLUNK_GROUP - group under which to run Splunk (default: splunk)
-	* SPLUNK_HOME - home directory where Splunk gets installed (default: /opt/splunk)
-	* SPLUNK_START_ARGS - arguments to pass into the Splunk start command; you must include '--accept-license' to start Splunk (default: none)
-	* SPLUNK_ROLE - the role of this Splunk instance (default: splunk_standalone)
-			Acceptable values:
-				- splunk_standalone
-				- splunk_search_head
-				- splunk_indexer
-				- splunk_deployer
-				- splunk_license_master
-				- splunk_cluster_master
-				- splunk_heavy_forwarder
-	* SPLUNK_LICENSE_URI - URI or local file path (absolute path in the container) to a Splunk license
-	* SPLUNK_STANDALONE_URL, SPLUNK_INDEXER_URL, ... - comma-separated list of resolvable aliases to properly bring-up a distributed environment.
-																										 This is optional for standalones, but required for multi-node Splunk deployments.
-	* SPLUNK_BUILD_URL - URL to a Splunk build which will be installed (instead of the image's default build)
-	* SPLUNK_APPS_URL - comma-separated list of URLs to Splunk apps which will be downloaded and installed
+  * SPLUNK_USER - user under which to run Splunk (default: splunk)
+  * SPLUNK_GROUP - group under which to run Splunk (default: splunk)
+  * SPLUNK_HOME - home directory where Splunk gets installed (default: /opt/splunk)
+  * SPLUNK_START_ARGS - arguments to pass into the Splunk start command; you must include '--accept-license' to start Splunk (default: none)
+  * SPLUNK_ROLE - the role of this Splunk instance (default: splunk_standalone)
+      Acceptable values:
+        - splunk_standalone
+        - splunk_search_head
+        - splunk_indexer
+        - splunk_deployer
+        - splunk_license_master
+        - splunk_cluster_master
+        - splunk_heavy_forwarder
+  * SPLUNK_LICENSE_URI - URI or local file path (absolute path in the container) to a Splunk license
+  * SPLUNK_STANDALONE_URL, SPLUNK_INDEXER_URL, ... - comma-separated list of resolvable aliases to properly bring-up a distributed environment.
+                                                     This is optional for standalones, but required for multi-node Splunk deployments.
+  * SPLUNK_BUILD_URL - URL to a Splunk build which will be installed (instead of the image's default build)
+  * SPLUNK_APPS_URL - comma-separated list of URLs to Splunk apps which will be downloaded and installed
 
 Examples:
-	* docker run -it -p 8000:8000 splunk/splunk start
-	* docker run -it -e SPLUNK_START_ARGS=--accept-license -p 8000:8000 -p 8089:8089 splunk/splunk start
-	* docker run -it -e SPLUNK_START_ARGS=--accept-license -e SPLUNK_LICENSE_URI=http://example.com/splunk.lic -p 8000:8000 splunk/splunk start
-	* docker run -it -e SPLUNK_START_ARGS=--accept-license -e SPLUNK_INDEXER_URL=idx1,idx2 -e SPLUNK_SEARCH_HEAD_URL=sh1,sh2 -e SPLUNK_ROLE=splunk_search_head --hostname sh1 --network splunknet --network-alias sh1 -e SPLUNK_PASSWORD=helloworld -e SPLUNK_LICENSE_URI=http://example.com/splunk.lic splunk/splunk start
+  * docker run -it -p 8000:8000 splunk/splunk start
+  * docker run -it -e SPLUNK_START_ARGS=--accept-license -p 8000:8000 -p 8089:8089 splunk/splunk start
+  * docker run -it -e SPLUNK_START_ARGS=--accept-license -e SPLUNK_LICENSE_URI=http://example.com/splunk.lic -p 8000:8000 splunk/splunk start
+  * docker run -it -e SPLUNK_START_ARGS=--accept-license -e SPLUNK_INDEXER_URL=idx1,idx2 -e SPLUNK_SEARCH_HEAD_URL=sh1,sh2 -e SPLUNK_ROLE=splunk_search_head --hostname sh1 --network splunknet --network-alias sh1 -e SPLUNK_PASSWORD=helloworld -e SPLUNK_LICENSE_URI=http://example.com/splunk.lic splunk/splunk start
 
 EOF
-		exit 1
+	exit 1
 }
 
 case "$1" in
