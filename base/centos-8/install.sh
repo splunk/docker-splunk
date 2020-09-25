327141cfc5e37246872dfdb4038389d1c5756320
James Rigassio <jrigassio@splunk.com>
2020-09-24 18:22:03 -0700

    py23 image and makefile centos 8

--- base/centos-8/install.sh
+++ base/centos-8/install.sh
@@ -15,11 +15,17 @@
 
 set -e
 
+yum -y install glibc-locale-source glibc-langpack-en
+
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 export LANG=en_US.utf8
 
-yum -y update && yum -y install wget sudo epel-release
-yum -y install busybox ansible python-requests python-jmespath
+yum -y update && yum -y install wget sudo epel-release make
+yum -y install ansible python3-requests python3-jmespath
+
+# Install busybox
+wget -O /bin/busybox https://busybox.net/downloads/binaries/1.28.1-defconfig-multiarch/busybox-`arch`
+chmod +x /bin/busybox
 
 # Install scloud
 wget -O /usr/bin/scloud.tar.gz ${SCLOUD_URL}
@@ -40,6 +46,9 @@ groupadd sudo
 echo "
 ## Allows people in group sudo to run all commands
 %sudo  ALL=(ALL)       ALL" >> /etc/sudoers
+
+# symlink for python3
+ln -s /bin/python3 /bin/python
 
 # Clean
 yum clean all
