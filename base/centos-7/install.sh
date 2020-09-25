41df38e8c9a0ddd4945c2c559f67ae7972e1ff47
Nelson Wang <nwang@splunk.com>
2020-09-25 15:07:42 -0700

    Support for createdefaults.py py2/py3

--- base/centos-7/install.sh
+++ base/centos-7/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2018 Splunk
+# Copyright 2018-2020 Splunk
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
