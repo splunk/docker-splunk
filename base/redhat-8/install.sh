a2398da1ffd31bf6acd2170c7759ed4b16dafe45
Alisha Mayor <amayor@splunk.com>
2021-08-20 01:35:06 -0700

    Release/822 + image fixes (#504)
    * Update ubi-minimal image and reinstall missing tzdata
    
    * Updating Makefile and changelog for Release/8.2.2

--- base/redhat-8/install.sh
+++ base/redhat-8/install.sh
@@ -16,21 +16,25 @@
 set -e
 
 # Generate UTF-8 char map and locale
-# reinstalling local en def for now, removed in minimal image https://bugzilla.redhat.com/show_bug.cgi?id=1665251
+# Reinstalling local English def for now, removed in minimal image: https://bugzilla.redhat.com/show_bug.cgi?id=1665251
 microdnf -y --nodocs install glibc-langpack-en
 
-#Currently there is no access to the UTF-8 char map, the following command is commented out until
-#the base container can generate the locale
-#localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
-#We get around the gen above by forcing the language install, and then point to it.
+# Currently there is no access to the UTF-8 char map. The following command is commented out until
+# the base container can generate the locale.
+# localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+# We get around the gen above by forcing the language install, and then pointing to it.
 export LANG=en_US.utf8
 
 # Install utility packages
-microdnf -y --nodocs install wget sudo shadow-utils procps tar tzdata make gcc \
+microdnf -y --nodocs install wget sudo shadow-utils procps tar make gcc \
                              openssl-devel bzip2-devel libffi-devel findutils
 # Patch security updates
-microdnf -y --nodocs update gnutls kernel-headers librepo libnghttp2 tzdata nettle \
-                            libpwquality libxml2 systemd-libs glib2 lz4-libs rpm rpm-libs
+microdnf -y --nodocs update gnutls kernel-headers librepo libnghttp2 nettle \
+                            libpwquality libxml2 systemd-libs glib2 lz4-libs \
+                            rpm rpm-libs
+
+# Reinstall tzdata (originally stripped from minimal image): https://bugzilla.redhat.com/show_bug.cgi?id=1903219
+microdnf -y --nodocs reinstall tzdata
 
 # Install Python and necessary packages
 PY_SHORT=${PYTHON_VERSION%.*}
@@ -51,15 +55,18 @@ make altinstall LDFLAGS="-Wl,--strip-all"
 rm -rf /tmp/pyinstall
 ln -sf /usr/bin/python${PY_SHORT} /usr/bin/python
 ln -sf /usr/bin/pip${PY_SHORT} /usr/bin/pip
+
 # Install splunk-ansible dependencies
 cd /
 pip -q --no-cache-dir install six wheel requests cryptography==3.3.2 ansible==3.4.0 urllib3==1.26.5 jmespath --upgrade
+
 # Remove tests packaged in python libs
 find /usr/lib/ -depth \( -type d -a -not -wholename '*/ansible/plugins/test' -a \( -name test -o -name tests -o -name idle_test \) \) -exec rm -rf '{}' \;
 find /usr/lib/ -depth \( -type f -a -name '*.pyc' -o -name '*.pyo' -o -name '*.a' \) -exec rm -rf '{}' \;
 find /usr/lib/ -depth \( -type f -a -name 'wininst-*.exe' \) -exec rm -rf '{}' \;
 ldconfig
 
+# Cleanup
 microdnf remove -y make gcc openssl-devel bzip2-devel libffi-devel findutils cpp binutils \
                    glibc-devel keyutils-libs-devel krb5-devel libcom_err-devel libselinux-devel \
                    libsepol-devel libverto-devel libxcrypt-devel pcre2-devel zlib-devel
@@ -70,9 +77,10 @@ wget -O /usr/bin/scloud.tar.gz ${SCLOUD_URL}
 tar -xf /usr/bin/scloud.tar.gz -C /usr/bin/
 rm /usr/bin/scloud.tar.gz
 
-# Install busybox direct from the multiarch since epel isn't availible yet for redhat8
+# Install busybox direct from the multiarch since EPEL isn't available yet for redhat8
 wget -O /bin/busybox https://busybox.net/downloads/binaries/1.28.1-defconfig-multiarch/busybox-`arch`
 chmod +x /bin/busybox
+
 # Enable busybox symlinks
 cd /bin
 BBOX_LINKS=( clear find diff hostname killall netstat nslookup ping ping6 readline route syslogd tail traceroute vi )
