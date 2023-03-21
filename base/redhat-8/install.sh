1362def0cbc020f610a317004df0cf8b140a33c9
Sujay <schathu@splunk.com>
2023-03-21 00:17:36 -0700

    updating setuptools

--- base/redhat-8/install.sh
+++ base/redhat-8/install.sh
@@ -62,7 +62,7 @@ ln -sf /usr/bin/pip${PY_SHORT} /usr/bin/pip
 # Install splunk-ansible dependencies
 cd /
 pip -q --no-cache-dir install six wheel requests ansible==7.0.0 Mako==1.2.2 avro==1.11.0 urllib3==1.26.5 setuptools==67.5.1 protobuf==3.18.3 lxml==4.9.1 cryptography==39.0.1 certifi==2022.12.07 jmespath --upgrade
-pip install Mako==1.2.2
+pip install setuptools==67.5.1
 
 # Remove tests packaged in python libs
 find /usr/lib/ -depth \( -type d -a -not -wholename '*/ansible/plugins/test' -a \( -name test -o -name tests -o -name idle_test \) \) -exec rm -rf '{}' \;
