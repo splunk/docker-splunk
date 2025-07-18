3e45bdda4785f956137d0a4802d107a6c46e7b4a
Jack Meixensperger <jackm@splunk.com>
2025-07-18 16:04:23 -0700

    keep language consistent across repo

--- splunk/common-files/entrypoint.sh
+++ splunk/common-files/entrypoint.sh
@@ -20,10 +20,10 @@ setup() {
 	# Check if the user accepted the license
 	if [[ "$SPLUNK_GENERAL_TERMS" != *"--accept-sgt-current-at-splunk-com"* ]] || [[ "$SPLUNK_START_ARGS" != *"--accept-license"* ]]; then
 		printf "License not accepted, please adjust SPLUNK_GENERAL_TERMS and/or SPLUNK_START_ARGS to indicate you have accepted the current/latest version of the license.\n"
-		printf "The license you are accepting is the Splunk General Terms, available here: https://www.splunk.com/en_us/legal/splunk-general-terms.html, as may be udpated from time to time.\n"
-		printf "Unless you have jointly executed with Splunk a negotiated version of these General Terms that explicitly supersedes this agreement, by accessing or using Splunk software, you are agreeing to the Splunk General Terms posted at the time of your access and use.\n"
+		printf "The license you are accepting is the current/latest version of the Splunk General Terms, available here: https://www.splunk.com/en_us/legal/splunk-general-terms.html, as may be updated from time to time.\n"
+		printf "Unless you have jointly executed with Splunk a negotiated version of these General Terms that explicitly supersedes this agreement, by accessing or using Splunk software, you are agreeing to the Splunk General Terms posted at the time of your access and use and acknowledging its applicability to the Splunk software.\n"
 		printf "Please read and make sure you agree to the Splunk General Terms before you access or use this software.\n"
-		printf "Only once you've done so should you include the '--accept-sgt-current-at-splunk-com' and '--accept-license' flags to indicate your acceptance of the Splunk General Terms and launch this software.\n"
+		printf "Only after doing so should you include the '--accept-sgt-current-at-splunk-com' and '--accept-license' flags to indicate your acceptance of the Splunk General Terms and launch this software.\n"
 		printf "For example: docker run -e SPLUNK_GENERAL_TERMS=--accept-sgt-current-at-splunk-com -e SPLUNK_START_ARGS=--accept-license -e SPLUNK_PASSWORD splunk/splunk\n\n"
 		printf "For additional information and examples, see the help: docker run -it splunk/splunk help\n"
 		exit 1
