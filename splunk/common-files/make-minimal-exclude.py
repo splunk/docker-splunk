5d94b728bb1e2986b73cb57d4db01dcbd91f1e1b
Huy Nguyen <huyn@splunk.com>
2026-09-10 14:20:39 -0700

    Exclude bin/jars for splunk 10.1+

--- splunk/common-files/make-minimal-exclude.py
+++ splunk/common-files/make-minimal-exclude.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import re, sys
+import re
+import sys
 
 EXCLUDE_V7 = """*-manifest
 */bin/installit.py
@@ -56,9 +57,9 @@ if major_version:
     elif int(major_version) == 10:
         if int(minor_version) >= 1:
             EXCLUDE_V7 = EXCLUDE_V7.replace('*/bin/node*', '')
-            EXCLUDE_V7 = EXCLUDE_V7.replace('*/lib/node_modules*', '')
-        if int(minor_version) >= 2:
+            EXCLUDE_V7 = EXCLUDE_V7.replace("*/lib/node_modules*", "")
             EXCLUDE_V7 = EXCLUDE_V7.replace('*/bin/jars/*', '')
+        if int(minor_version) >= 2:
             EXCLUDE_V7 = EXCLUDE_V7.replace('*/etc/apps/splunk_archiver*', '')
         EXCLUDE_V7 = EXCLUDE_V7.replace('*/bin/jsmin*', '')
     elif int(major_version) > 10:
